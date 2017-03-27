adf94ff855528845b324effbec4623e4d93c990d
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-03-26 22:11:44 -0700

    Load individual questions on their own routes

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  model(params) {
+    return this.store.findRecord("question", params.question_id);
+  },
+  actions: {
+    delete(question) {
+      question.destroyRecord();
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -4,4 +4,9 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord("question", params.question_id);
   },
+  actions: {
+    delete(question) {
+      this.sendAction("delete", question);
+    }
+  }
 });
